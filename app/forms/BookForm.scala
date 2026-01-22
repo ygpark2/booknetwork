3c31e4aaafff51ac27533c6fa96df02df754f739
Young Gyu Park <ygpark2@gmail.com>
2026-01-22 23:41:55 +0900

    adding bookform object

--- app/forms/BookForm.scala
+++ app/forms/BookForm.scala
@@ -8,3 +8,11 @@ case class BookForm(
   description: Option[String],
   metadata: BookMetadata
 )
+
+object BookForm {
+  def apply(title: String, author: String, description: Option[String], metadata: BookMetadata): BookForm =
+    new BookForm(title, author, description, metadata)
+
+  def unapply(form: BookForm): Option[(String, String, Option[String], BookMetadata)] =
+    Some((form.title, form.author, form.description, form.metadata))
+}
